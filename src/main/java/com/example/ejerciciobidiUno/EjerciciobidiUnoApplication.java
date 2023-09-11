483ea54b0ddb19a12e237394108e440d10252d34
Lautaro <lautaro2205@gmail.com>
2023-09-11 09:24:03 -0300

    Subida

---
+++ src/main/java/com/example/ejerciciobidiUno/EjerciciobidiUnoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciobidiUno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciobidiUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciobidiUnoApplication.class, args);
+	}
+
+}
